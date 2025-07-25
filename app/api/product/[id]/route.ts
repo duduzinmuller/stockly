21a4720ef3f06c1a4bf3992b1925efdaf454ad1a
duduzinmuller <eduardomuller989@gmail.com>
2025-07-25 11:24:48 -0400

    feat: add dynamic routes handlers

---
+++ app/api/product/[id]/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/app/_lib/prisma";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("test");
+  console.log({ query });
+  const productId = params.id;
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!product) {
+    return Response.json(
+      { message: "Produto não encontrado" },
+      { status: 404 },
+    );
+  }
+
+  return Response.json(product, { status: 200 });
+}
+
+export async function DELETE({ params }: { params: { id: string } }) {
+  await db.product.delete({
+    where: {
+      id: params.id,
+    },
+  });
+
+  return Response.json({}, { status: 200 });
+}
