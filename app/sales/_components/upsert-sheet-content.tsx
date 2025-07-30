77b94f3b7dedaf734990a72522ce75ca326f4657
duduzinmuller <eduardomuller989@gmail.com>
2025-07-30 14:29:50 -0400

    feat: add initial sale creation component

---
+++ app/sales/_components/upsert-sheet-content.tsx
@@ -0,0 +1,205 @@
+"use client";
+
+import { Button } from "@/app/_components/ui/button";
+import { Combobox, ComboboxOption } from "@/app/_components/ui/combobox";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import { Input } from "@/app/_components/ui/input";
+import {
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/app/_components/ui/sheet";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/app/_components/ui/table";
+import { formatCurrency } from "@/app/_helpers/format-currency";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Product } from "@prisma/client";
+import { PlusIcon } from "lucide-react";
+import { useMemo, useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { NumericFormat } from "react-number-format";
+import z from "zod";
+
+const formSchema = z.object({
+  productId: z.string().uuid({
+    message: "O produto é obrigatório",
+  }),
+  quantity: z.number().int().positive(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+interface UpsertSheetContentProps {
+  products: Product[];
+  productsOptions: ComboboxOption[];
+}
+
+interface SelectedProduct {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+const UpsertSheetContent = ({
+  products,
+  productsOptions,
+}: UpsertSheetContentProps) => {
+  const [selectedProducts, setSelectedProducts] = useState<SelectedProduct[]>(
+    [],
+  );
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      productId: "",
+      quantity: 1,
+    },
+  });
+
+  const onSubmit = (data: FormSchema) => {
+    const selectedProduct = products.find(
+      (product) => product.id === data.productId,
+    );
+    if (!selectedProduct) return;
+    setSelectedProducts((currentProducts) => {
+      const existingProduct = currentProducts.find(
+        (product) => product.id === selectedProduct.id,
+      );
+      if (existingProduct) {
+        return currentProducts.map((product) => {
+          if (product.id === selectedProduct.id) {
+            return {
+              ...product,
+              quantity: product.quantity + data.quantity,
+            };
+          }
+          return product;
+        });
+      }
+      return [
+        ...currentProducts,
+        {
+          ...selectedProduct,
+          price: Number(selectedProduct.price),
+          quantity: data.quantity,
+        },
+      ];
+    });
+    console.log(data);
+    form.reset();
+  };
+
+  const productTotal = useMemo(() => {
+    return selectedProducts.reduce((acc, product) => {
+      return acc + product.price * product.quantity;
+    }, 0);
+  }, [selectedProducts]);
+  return (
+    <SheetContent className="!max-w-[700px]">
+      <SheetHeader>
+        <SheetTitle>Adicionar venda</SheetTitle>
+        <SheetDescription>
+          Insira as informações da venda abaixo
+        </SheetDescription>
+      </SheetHeader>
+      <Form {...form}>
+        <form className="space-y-6 py-6" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="productId"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Produto</FormLabel>
+                <FormControl>
+                  <Combobox
+                    placeholder="Selecione um produto"
+                    options={productsOptions}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="quantity"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Quantidade</FormLabel>
+                <FormControl>
+                  <NumericFormat
+                    allowNegative={false}
+                    decimalScale={0}
+                    customInput={Input}
+                    placeholder="Digite a quantidade do produto"
+                    onValueChange={(values) =>
+                      field.onChange(values.floatValue)
+                    }
+                    {...field}
+                    onChange={() => {}}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button variant="secondary" className="w-full gap-2">
+            <PlusIcon size={20} />
+            Adicionar produto á venda
+          </Button>
+
+          <Table>
+            <TableCaption>Lista dos produtos adicionados á venda</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Produto</TableHead>
+                <TableHead>Preço Unítario</TableHead>
+                <TableHead>Quantidade</TableHead>
+                <TableHead>Total</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {selectedProducts.map((product) => (
+                <TableRow key={product.id}>
+                  <TableCell>{product.name}</TableCell>
+                  <TableCell>{formatCurrency(product.price)}</TableCell>
+                  <TableCell>{product.quantity}</TableCell>
+                  <TableCell>
+                    {formatCurrency(product.price * product.quantity)}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+            <TableFooter>
+              <TableRow>
+                <TableCell colSpan={3}>Total</TableCell>
+                <TableCell>{formatCurrency(productTotal)}</TableCell>
+              </TableRow>
+            </TableFooter>
+          </Table>
+        </form>
+      </Form>
+    </SheetContent>
+  );
+};
+
+export default UpsertSheetContent;
